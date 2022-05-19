0c72a238f0338657b1829e44b6c7e8cca90a6a27
Death <ricardo.pajaro.bc2d@gmail.com>
2022-05-19 08:30:48 -0500

    adding mini map to config screen

--- app/src/main/java/com/nullpointer/runningcompose/ui/screens/config/components/MapConfig.kt
+++ app/src/main/java/com/nullpointer/runningcompose/ui/screens/config/components/MapConfig.kt
@@ -1,27 +1,24 @@
 package com.nullpointer.runningcompose.ui.screens.config.components
 
 import android.content.res.Configuration
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.*
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.LatLngBounds
+import com.google.android.gms.maps.model.MapStyleOptions
+import com.google.maps.android.compose.*
 import com.nullpointer.runningcompose.R
 import com.nullpointer.runningcompose.models.config.MapConfig
 import com.nullpointer.runningcompose.models.types.MapStyle
 import com.nullpointer.runningcompose.ui.screens.config.components.share.SelectOptionConfig
 import com.nullpointer.runningcompose.ui.screens.config.components.share.TitleConfig
+import timber.log.Timber
 
 
 @Composable
@@ -35,8 +32,8 @@ fun MapSettings(
         TitleConfig(text = stringResource(R.string.title_config_map))
         when (orientation) {
             Configuration.ORIENTATION_PORTRAIT -> {
-                MapFromConfig()
                 configMap?.let { mapConfig ->
+                    MapFromConfig(mapConfig = configMap)
                     SelectMapStyle(currentStyle = mapConfig.style, changeStyleMap = changeStyleMap)
                     Spacer(modifier = Modifier.height(10.dp))
                     SelectMapWeight(currentWeightMap = mapConfig.weight,
@@ -45,12 +42,12 @@ fun MapSettings(
             }
             else -> {
                 Row(verticalAlignment = Alignment.CenterVertically) {
-                    MapFromConfig(modifier = Modifier.weight(.5f))
-                    Column(
-                        verticalArrangement = Arrangement.Center,
-                        modifier = Modifier.weight(.5f),
-                    ) {
-                        configMap?.let { mapConfig ->
+                    configMap?.let { mapConfig ->
+                        MapFromConfig(mapConfig = configMap, modifier = Modifier.weight(.5f))
+                        Column(
+                            verticalArrangement = Arrangement.Center,
+                            modifier = Modifier.weight(.5f),
+                        ) {
                             SelectMapStyle(currentStyle = mapConfig.style,
                                 changeStyleMap = changeStyleMap)
                             Spacer(modifier = Modifier.height(10.dp))
@@ -66,14 +63,68 @@ fun MapSettings(
 
 @Composable
 private fun MapFromConfig(
+    mapConfig: MapConfig,
     modifier: Modifier = Modifier,
 ) {
-    Box(modifier = modifier
-        .fillMaxWidth()
-        .aspectRatio(2f)
-        .padding(vertical = 10.dp, horizontal = 20.dp)
-        .background(
-            Color.Gray))
+    val context = LocalContext.current
+    val uiSettings by remember {
+        mutableStateOf(MapUiSettings(
+            myLocationButtonEnabled = false,
+            compassEnabled = false,
+            indoorLevelPickerEnabled = false,
+            mapToolbarEnabled = false,
+            rotationGesturesEnabled = false,
+            scrollGesturesEnabled = false,
+            scrollGesturesEnabledDuringRotateOrZoom = false,
+            tiltGesturesEnabled = false,
+            zoomControlsEnabled = false,
+            zoomGesturesEnabled = false
+        ))
+    }
+    var properties by remember {
+        mutableStateOf(
+            MapProperties(
+                mapStyleOptions = MapStyleOptions.loadRawResourceStyle(context,
+                    mapConfig.style.styleRawRes),
+            )
+        )
+    }
+    val listPoints by remember {
+        mutableStateOf(listOf(
+            LatLng(53.3477, -6.2597),
+            LatLng(51.5008, -0.1224),
+            LatLng(48.8567, 2.3508),
+            LatLng(52.5166, 13.3833),
+        ))
+    }
+    val bounds by remember {
+        mutableStateOf(LatLngBounds.builder().let {
+            listPoints.forEach(it::include)
+            it.build()
+        })
+    }
+    val cameraPositionState = rememberCameraPositionState()
+    LaunchedEffect(key1 = mapConfig) {
+        properties = properties.copy(
+            mapStyleOptions = MapStyleOptions.loadRawResourceStyle(context,
+                mapConfig.style.styleRawRes)
+        )
+    }
+
+    GoogleMap(
+        modifier = modifier
+            .fillMaxWidth()
+            .aspectRatio(2f)
+            .padding(vertical = 10.dp, horizontal = 20.dp),
+        cameraPositionState = cameraPositionState,
+        properties = properties,
+        uiSettings = uiSettings,
+        onMapLoaded = {
+            cameraPositionState.move(CameraUpdateFactory.newLatLngBounds(bounds, 10))
+        }
+    ) {
+        Polyline(points = listPoints, width = mapConfig.weight.toFloat())
+    }
 }
 
 @Composable
@@ -81,7 +132,7 @@ private fun SelectMapWeight(
     currentWeightMap: Int,
     changeWeight: (Int) -> Unit,
 ) {
-    val listWeight = remember { listOf(1, 2, 3, 4, 5, 6, 7) }
+    val listWeight = remember { listOf(3, 4, 5, 6, 7, 8, 9, 10) }
     SelectOptionConfig(
         textField = stringResource(R.string.title_weight_map_line),
         selected = currentWeightMap.toString(),
--- app/src/main/java/com/nullpointer/runningcompose/ui/screens/tracking/TrackingScreen.kt
+++ app/src/main/java/com/nullpointer/runningcompose/ui/screens/tracking/TrackingScreen.kt
@@ -4,9 +4,13 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.LatLngBounds
+import com.google.android.gms.maps.model.PolylineOptions
 import com.google.maps.android.compose.GoogleMap
+import com.google.maps.android.compose.Polyline
 import com.google.maps.android.compose.rememberCameraPositionState
 import com.nullpointer.runningcompose.ui.share.ToolbarBack
 import com.ramcosta.composedestinations.annotation.Destination
@@ -22,13 +26,25 @@ fun TrackingScreen(
             ToolbarBack(title = "Seguimiento", actionBack = navigator::popBackStack)
         }
     ) {
-        val singapore = LatLng(1.35, 103.87)
-        val cameraPositionState = rememberCameraPositionState {
-            position = CameraPosition.fromLatLngZoom(singapore, 10f)
+        val listPoints = listOf(
+            LatLng(53.3477, -6.2597),
+            LatLng(51.5008, -0.1224),
+            LatLng(48.8567, 2.3508),
+            LatLng(52.5166, 13.3833),
+        )
+        val bounds = LatLngBounds.builder().let {
+            listPoints.forEach(it::include)
+            it.build()
         }
+        val cameraPositionState = rememberCameraPositionState ()
         GoogleMap(
             modifier = Modifier.fillMaxSize(),
-            cameraPositionState = cameraPositionState
-        )
+            cameraPositionState = cameraPositionState,
+            onMapLoaded = {
+                cameraPositionState.move(CameraUpdateFactory.newLatLngBounds(bounds, 10))
+            }
+        ) {
+            Polyline(points = listPoints)
+        }
     }
 }
