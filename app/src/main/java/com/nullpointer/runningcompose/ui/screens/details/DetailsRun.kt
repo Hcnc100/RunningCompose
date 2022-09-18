8569238db4ca3d56368b094af5d8adf2ea5c0dc3
Death <ricardo.pajaro.bc2d@gmail.com>
2022-09-18 15:02:05 -0500

    refactor DetailsRun.kt

--- app/src/main/java/com/nullpointer/runningcompose/ui/screens/details/DetailsRun.kt
+++ app/src/main/java/com/nullpointer/runningcompose/ui/screens/details/DetailsRun.kt
@@ -1,28 +1,20 @@
 package com.nullpointer.runningcompose.ui.screens.details
 
-import android.content.Context
 import android.content.res.Configuration.ORIENTATION_LANDSCAPE
 import android.content.res.Configuration.ORIENTATION_PORTRAIT
-import androidx.compose.animation.animateContentSize
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.*
-import androidx.compose.runtime.*
+import androidx.compose.material.Scaffold
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.nullpointer.runningcompose.R
-import com.nullpointer.runningcompose.core.utils.*
 import com.nullpointer.runningcompose.models.Run
 import com.nullpointer.runningcompose.models.types.MetricType
 import com.nullpointer.runningcompose.ui.navigation.MainNavGraph
+import com.nullpointer.runningcompose.ui.screens.details.components.StatisticsRun
 import com.nullpointer.runningcompose.ui.screens.runs.componets.MapRunItem
 import com.nullpointer.runningcompose.ui.share.ToolbarBack
 import com.nullpointer.runningcompose.ui.states.OrientationScreenState
@@ -39,12 +31,7 @@ fun DetailsRun(
     navigator: DestinationsNavigator,
     detailsState: OrientationScreenState = rememberOrientationScreenState()
 ) {
-    var isExpanded by remember {
-        mutableStateOf(false)
-    }
-    val iconExpanded = remember(isExpanded) {
-        if (isExpanded) R.drawable.ic_arrow_up else R.drawable.ic_arrow_down
-    }
+
     Scaffold(
         topBar = {
             ToolbarBack(
@@ -63,42 +50,17 @@ fun DetailsRun(
                             .fillMaxSize()
                             .padding(padding)
                     )
-                    Card(
+                    StatisticsRun(
+                        fontSizeBody = 12.sp,
+                        fontSizeTitle = 14.sp,
+                        itemRun = itemsRun,
+                        metricType = metricType,
+                        isStatisticsExpanded = true,
                         modifier = Modifier
+                            .width(250.dp)
                             .padding(5.dp)
                             .align(Alignment.TopStart)
-                            .width(250.dp)
-                            .animateContentSize()
-                            .clickable { isExpanded = !isExpanded },
-                        shape = MaterialTheme.shapes.small
-                    ) {
-                        Column {
-                            Row(
-                                modifier = Modifier.fillMaxWidth(),
-                                verticalAlignment = Alignment.CenterVertically,
-                                horizontalArrangement = Arrangement.Center
-                            ) {
-                                Text(
-                                    fontSize = 16.sp,
-                                    textAlign = TextAlign.Center,
-                                    modifier = Modifier.padding(5.dp),
-                                    style = MaterialTheme.typography.h6,
-                                    text = stringResource(id = R.string.title_screen_statistics)
-                                )
-                                Spacer(modifier = Modifier.width(5.dp))
-                                Icon(
-                                    painter = painterResource(id = iconExpanded),
-                                    contentDescription = stringResource(R.string.description_arrow_statistics)
-                                )
-                            }
-                            if (isExpanded)
-                                InfoRun(
-                                    itemRun = itemsRun,
-                                    modifier = Modifier.padding(10.dp),
-                                    metricType = metricType
-                                )
-                        }
-                    }
+                    )
 
                 }
             }
@@ -112,80 +74,19 @@ fun DetailsRun(
                             .fillMaxWidth()
                             .weight(6.5f)
                     )
-                    Card(
+                    StatisticsRun(
+                        fontSizeTitle = 24.sp,
+                        fontSizeBody = 16.sp,
+                        itemRun = itemsRun,
+                        metricType = metricType,
+                        isStatisticsExpanded = false,
                         modifier = Modifier
-                            .padding(10.dp)
-                            .weight(3.5f)
-                            .fillMaxWidth(),
-                        shape = MaterialTheme.shapes.small
-                    ) {
-                        Column {
-                            Text(
-                                text = stringResource(id = R.string.title_screen_statistics),
-                                modifier = Modifier
-                                    .fillMaxWidth()
-                                    .padding(5.dp),
-                                textAlign = TextAlign.Center,
-                                style = MaterialTheme.typography.h5
-                            )
-                            InfoRun(
-                                itemRun = itemsRun,
-                                modifier = Modifier.padding(10.dp),
-                                metricType = metricType
-                            )
-                        }
-                    }
+                            .fillMaxWidth()
+                            .padding(5.dp)
+                    )
                 }
             }
         }
     }
 }
 
-@Composable
-private fun InfoRun(
-    itemRun: Run,
-    metricType: MetricType,
-    modifier: Modifier = Modifier,
-    context: Context = LocalContext.current,
-) {
-    val date = remember { itemRun.timestamp.toDateFormat() }
-    val timeDay = remember { itemRun.timestamp.toDateOnlyTime(context) }
-    val speed = remember { itemRun.avgSpeedInMeters.toAVGSpeed(metricType) }
-    val distance = remember { itemRun.distanceInMeters.toMeters(metricType) }
-    val calories = remember { itemRun.caloriesBurned.toCaloriesBurned(metricType) }
-    val timeRun = remember { itemRun.timeRunInMillis.toFullFormatTime(true) }
-
-    Column(modifier = modifier.verticalScroll(rememberScrollState())) {
-        TitleAndInfo(stringResource(R.string.item_title_date), date)
-        TitleAndInfo(stringResource(R.string.item_title_hour), timeDay)
-        TitleAndInfo(stringResource(R.string.item_title_duration), timeRun)
-        TitleAndInfo(stringResource(R.string.item_title_distance), distance)
-        TitleAndInfo(stringResource(R.string.item_title_speed), speed)
-        TitleAndInfo(stringResource(R.string.item_title_calories), calories)
-    }
-
-}
-
-@Composable
-private fun TitleAndInfo(
-    title: String,
-    info: String
-) {
-    Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceBetween) {
-        DetailsRunText(text = title)
-        Spacer(modifier = Modifier.width(10.dp))
-        DetailsRunText(text = info)
-    }
-}
-
-@Composable
-private fun DetailsRunText(
-    text: String,
-    modifier: Modifier = Modifier
-) {
-    Text(
-        modifier = modifier,
-        text = text,
-        style = MaterialTheme.typography.body1,
-    )
-}
---
+++ app/src/main/java/com/nullpointer/runningcompose/ui/screens/details/components/StatisticsRun.kt
@@ -0,0 +1,140 @@
+package com.nullpointer.runningcompose.ui.screens.details.components
+
+import android.content.Context
+import androidx.compose.animation.animateContentSize
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.Card
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.dp
+import com.nullpointer.runningcompose.R
+import com.nullpointer.runningcompose.core.utils.*
+import com.nullpointer.runningcompose.models.Run
+import com.nullpointer.runningcompose.models.types.MetricType
+
+@Composable
+fun StatisticsRun(
+    itemRun: Run,
+    fontSizeBody: TextUnit,
+    metricType: MetricType,
+    fontSizeTitle: TextUnit,
+    modifier: Modifier = Modifier,
+    isStatisticsExpanded: Boolean
+) {
+    var isExpanded by rememberSaveable { mutableStateOf(false) }
+
+    Card(
+        modifier = modifier
+            .animateContentSize()
+            .clickable { if (isStatisticsExpanded) isExpanded = !isExpanded },
+        shape = MaterialTheme.shapes.small
+    ) {
+        Column {
+            TitleStatistics(
+                isExpanded = isExpanded,
+                fontSizeTitle = fontSizeTitle,
+                isStatisticsExpanded = isStatisticsExpanded
+            )
+            if (isExpanded || !isStatisticsExpanded)
+                InfoRun(
+                    itemRun = itemRun,
+                    modifier = Modifier.padding(10.dp),
+                    metricType = metricType,
+                    fontSize = fontSizeBody
+                )
+        }
+    }
+}
+
+
+@Composable
+fun TitleStatistics(
+    isExpanded: Boolean,
+    fontSizeTitle: TextUnit,
+    isStatisticsExpanded: Boolean
+) {
+    val iconExpanded = rememberSaveable(isExpanded) {
+        if (isExpanded) R.drawable.ic_arrow_up else R.drawable.ic_arrow_down
+    }
+
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.Center
+    ) {
+        Text(
+            textAlign = TextAlign.Center,
+            modifier = Modifier.padding(5.dp),
+            text = stringResource(id = R.string.title_screen_statistics),
+            style = MaterialTheme.typography.h5.copy(fontSize = fontSizeTitle)
+        )
+        if (isStatisticsExpanded) {
+            Spacer(modifier = Modifier.width(5.dp))
+            Icon(
+                painter = painterResource(id = iconExpanded),
+                contentDescription = stringResource(R.string.description_arrow_statistics)
+            )
+        }
+    }
+}
+
+@Composable
+private fun InfoRun(
+    itemRun: Run,
+    fontSize: TextUnit,
+    metricType: MetricType,
+    modifier: Modifier = Modifier,
+    context: Context = LocalContext.current
+) {
+    val date = remember { itemRun.timestamp.toDateFormat() }
+    val timeDay = remember { itemRun.timestamp.toDateOnlyTime(context) }
+    val speed = remember { itemRun.avgSpeedInMeters.toAVGSpeed(metricType) }
+    val distance = remember { itemRun.distanceInMeters.toMeters(metricType) }
+    val calories = remember { itemRun.caloriesBurned.toCaloriesBurned(metricType) }
+    val timeRun = remember { itemRun.timeRunInMillis.toFullFormatTime(true) }
+
+    Column(modifier = modifier.verticalScroll(rememberScrollState())) {
+        TitleAndInfo(stringResource(R.string.item_title_date), date, fontSize)
+        TitleAndInfo(stringResource(R.string.item_title_hour), timeDay, fontSize)
+        TitleAndInfo(stringResource(R.string.item_title_duration), timeRun, fontSize)
+        TitleAndInfo(stringResource(R.string.item_title_distance), distance, fontSize)
+        TitleAndInfo(stringResource(R.string.item_title_speed), speed, fontSize)
+        TitleAndInfo(stringResource(R.string.item_title_calories), calories, fontSize)
+    }
+
+}
+
+@Composable
+private fun TitleAndInfo(
+    info: String,
+    title: String,
+    fontSize: TextUnit
+) {
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        Text(
+            text = info,
+            style = MaterialTheme.typography.body1.copy(fontSize = fontSize),
+        )
+        Spacer(modifier = Modifier.width(10.dp))
+        Text(
+            text = title,
+            style = MaterialTheme.typography.body1.copy(fontSize = fontSize),
+        )
+    }
+}
