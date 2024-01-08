dcaf303eb0a71d10cf59553a8490d55cb546dbce
ricardop <rpajaro@sinprol.com.mx>
2024-01-08 17:54:39 -0600

    mod InfoRun item

--- app/src/main/java/com/nullpointer/runningcompose/ui/screens/runs/componets/ItemRun.kt
+++ app/src/main/java/com/nullpointer/runningcompose/ui/screens/runs/componets/ItemRun.kt
@@ -3,7 +3,6 @@ package com.nullpointer.runningcompose.ui.screens.runs.componets
 import android.content.Context
 import androidx.compose.animation.animateColorAsState
 import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -21,14 +20,16 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
 import coil.compose.rememberAsyncImagePainter
 import coil.request.ImageRequest
 import com.nullpointer.runningcompose.R
 import com.nullpointer.runningcompose.core.utils.*
 import com.nullpointer.runningcompose.models.data.RunData
-import com.nullpointer.runningcompose.models.entities.RunEntity
+import com.nullpointer.runningcompose.models.data.config.MapConfig
 import com.nullpointer.runningcompose.models.types.MetricType
 import com.nullpointer.runningcompose.ui.screens.runs.ActionRun
 
@@ -62,18 +63,19 @@ fun ItemRun(
                 onLongClick = { if (!isSelectEnable) actionRun(ActionRun.SELECT) }
             )
     ) {
-        Row(modifier = Modifier
-            .padding(10.dp)
-            .height(150.dp)) {
+        Row(
+            modifier = Modifier
+                .padding(10.dp)
+                .height(150.dp),
+            horizontalArrangement = Arrangement.spacedBy(20.dp)
+        ) {
             // * waiting to take snapshot for maps compose
             ImageRun(
                 data = itemRunEntity.pathImgRun,
                 modifier = Modifier.weight(.5f)
             )
-
-            Spacer(modifier = Modifier.width(20.dp))
             InfoRun(
-                itemRunEntity = itemRunEntity,
+                itemRun = itemRunEntity,
                 modifier = Modifier.weight(.5f),
                 metricType = metricType
             )
@@ -83,7 +85,7 @@ fun ItemRun(
 
 @Composable
 private fun ImageRun(
-    data: Any?,
+    data: String?,
     modifier: Modifier = Modifier
 ) {
 
@@ -94,10 +96,11 @@ private fun ImageRun(
             .build(),
         placeholder = painterResource(id = R.drawable.ic_map),
         error = painterResource(id = R.drawable.ic_broken_image),
+        contentScale = ContentScale.Crop
     )
 
-    Image(
-        painter = painter,
+    AsyncImage(
+        model = data,
         contentDescription = stringResource(R.string.description_current_run_img),
         contentScale = if (painter.isSuccess) ContentScale.Crop else ContentScale.Fit,
         colorFilter = if(painter.isSuccess) null else ColorFilter.tint(getGrayColor()),
@@ -110,24 +113,23 @@ private fun ImageRun(
 
 @Composable
 private fun InfoRun(
-    itemRunEntity: RunData,
+    itemRun: RunData,
     metricType: MetricType,
     modifier: Modifier = Modifier,
     context:Context=LocalContext.current
 ) {
-    val date = remember { itemRunEntity.timestamp.toDateFormat() }
-    val timeDay = remember { itemRunEntity.timestamp.toDateOnlyTime(context) }
-    val timeRun = remember { itemRunEntity.timeRunInMillis.toFullFormatTime(false) }
-    val distance = remember { itemRunEntity.distanceInMeters.toMeters(metricType) }
-    val calories = remember { itemRunEntity.caloriesBurned.toCaloriesBurned(metricType) }
-    val speed = remember { itemRunEntity.avgSpeedInMeters.toAVGSpeed(metricType) }
+    val date = remember { itemRun.timestamp.toDateFormat() }
+    val timeDay = remember { itemRun.timestamp.toDateOnlyTime(context) }
+    val timeRun = remember { itemRun.timeRunInMillis.toFullFormatTime(false) }
+    val distance = remember { itemRun.distanceInMeters.toMeters(metricType) }
+    val calories = remember { itemRun.caloriesBurned.toCaloriesBurned(metricType) }
+    val speed = remember { itemRun.avgSpeedInMeters.toAVGSpeed(metricType) }
 
     Row(
-        modifier = modifier
-            .padding(vertical = 10.dp)
-            .fillMaxWidth()
+        modifier = modifier.padding(vertical = 10.dp),
+        horizontalArrangement = Arrangement.spacedBy(10.dp)
     ) {
-        Column(verticalArrangement = Arrangement.SpaceAround, modifier = Modifier.fillMaxHeight()) {
+        Column(verticalArrangement = Arrangement.SpaceAround) {
             DetailsRunText(text = stringResource(R.string.item_title_date))
             DetailsRunText(text = stringResource(R.string.item_title_hour))
             DetailsRunText(text = stringResource(R.string.item_title_duration))
@@ -135,8 +137,7 @@ private fun InfoRun(
             DetailsRunText(text = stringResource(R.string.item_title_speed))
             DetailsRunText(text = stringResource(R.string.item_title_calories))
         }
-        Spacer(modifier = Modifier.width(10.dp))
-        Column(verticalArrangement = Arrangement.SpaceAround, modifier = Modifier.fillMaxHeight()) {
+        Column(verticalArrangement = Arrangement.SpaceAround) {
             DetailsRunText(text = date)
             DetailsRunText(text = timeDay)
             DetailsRunText(text = timeRun)
@@ -145,6 +146,25 @@ private fun InfoRun(
             DetailsRunText(text = calories)
         }
     }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun InfoRunPreview(){
+    InfoRun(
+        itemRun = RunData(
+            id = 1,
+            avgSpeedInMeters = 100F,
+            caloriesBurned = 10F,
+            distanceInMeters = 1000F,
+            listPolyLineEncode = emptyList(),
+            pathImgRun = null,
+            timeRunInMillis = 10,
+            mapConfig = MapConfig(),
+            timestamp = 10
+        ),
+        metricType = MetricType.Meters
+    )
 }
 
 @Composable
@@ -158,4 +178,12 @@ private fun DetailsRunText(
         maxLines = 1,
         overflow = TextOverflow.Ellipsis
     )
+}
+
+@Preview(
+    showBackground = true
+)
+@Composable
+fun DetailsRunTextPreview() {
+    DetailsRunText(text = "Este es un texto de prueba")
 }
