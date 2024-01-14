686445af7d290cd4bbaa8877abfe26fb04e4e86d
Hcnc100 <ricardo.pajaro.bc2d@gmail.com>
2024-01-14 14:46:25 -0600

    change build.gradle to build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.nullpointer.runningcompose"
+        applicationId = "com.nullpointer.running"
         minSdk = 21
         targetSdk = 34
         versionCode = 3
