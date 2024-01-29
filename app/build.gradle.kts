c0c46c7b622680dd22d5e9281dd58a7e3b5b354c
Hcnc100 <ricardo.pajaro.bc2d@gmail.com>
2024-01-29 06:38:09 -0600

    change theme and name app label

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "com.nullpointer.running"
         minSdk = 21
         targetSdk = 34
-        versionCode = 3
-        versionName = "3.0.0"
+        versionCode = 4
+        versionName = "3.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
